7dea50cbba20ea3f4cda00d7026f539a26a025ae
LotusSerperior <roboticjo123@gmail.com>
2016-12-19 22:51:53 +1100

    Create pokedex.js

---
+++ mods/serpmons/pokedex.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.BattlePokedex = {
+  combzaprior: {
+		num: 10008,
+		species: "Electric Warrior",
+		types: ["Electric", "Fighting"],
+		baseStats: {hp: 83, atk: 45, def: 79, spa: 130, spd: 100, spe: 92},
+		abilities: {0: "Lightningrod", 1: "Galvanise", H: "Combat Artist"},
+		heightm: 3.2,
+		weightkg: 49.4,
+		color: "Black",
+		eggGroups: ["Human-Like", "Field"],
+	},
+  };
