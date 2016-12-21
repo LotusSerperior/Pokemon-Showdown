d61c459db7174671a9530c5c30aea4498d20db45
LotusSerperior <roboticjo123@gmail.com>
2016-12-21 22:48:38 +1100

    Update formats-data.js

--- mods/serpmons/formats-data.js
+++ mods/serpmons/formats-data.js
@@ -5,4 +5,8 @@ exports.BattleFormatsData = {
 		randomBattleMoves: ["thunderbolt", "quickspark", "darkpulse", "manipulatingblow"],
 		tier: "New",
     },
+	testee: {
+		randomBattleMoves: ["icebeam", "recover", "brickbreak", "iceshard"],
+		tier: "New",
+	},
 };
