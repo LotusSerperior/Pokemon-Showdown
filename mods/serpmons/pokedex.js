0cddc24af1d67e464d9a977e1f0f669270bdab2b
LotusSerperior <roboticjo123@gmail.com>
2016-12-21 22:43:10 +1100

    Update pokedex.js

--- mods/serpmons/pokedex.js
+++ mods/serpmons/pokedex.js
@@ -1,12 +1,23 @@
 'use strict';
 
 exports.BattlePokedex = {
-  combzaprior: {
+	combzaprior: {
 		num: 10008,
 		species: "Combzaprior",
 		types: ["Electric", "Fighting"],
 		baseStats: {hp: 83, atk: 45, def: 79, spa: 130, spd: 100, spe: 92},
 		abilities: {0: "Lightningrod", 1: "Galvanise", H: "Combat Artist"},
+		heightm: 3.2,
+		weightkg: 49.4,
+		color: "Black",
+		eggGroups: ["Human-Like", "Field"],
+	},
+	testee: {
+		num: 10009,
+		species: "Testee",
+		types: ["Ice"],
+		baseStats: {hp: 100, atk: 80, def: 100, spa: 70, spd: 90, spe: 65},
+		abilities: {0: "Testmabob"},
 		heightm: 3.2,
 		weightkg: 49.4,
 		color: "Black",
